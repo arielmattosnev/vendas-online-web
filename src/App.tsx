1aa1cadc22dcc19c1e17f56e7116ae8edb642598
Arielmtts7 <arielmattosnev@gmail.com>
2023-03-07 20:28:06 -0300

    feat: insert eslint in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <div className="App">
-     <h1>Olá mundo!</h1>
+      <h1>Olá mundo!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
