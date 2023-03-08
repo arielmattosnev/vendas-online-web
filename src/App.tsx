f0db1f94bf289b4df6f711008d503752a0cb0b46
Arielmtts7 <arielmattosnev@gmail.com>
2023-03-07 21:03:47 -0300

    feat: insert antd and styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import './App.css';
 
+import { Button } from 'antd';
+import { useState } from 'react';
+import styled from 'styled-components';
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
+    <StyledDiv isBlue={count > 4} className="app">
       <h1>Olá mundo!</h1>
-    </div>
+      <p>Contagem: {count}</p>
+      <Button type="primary" onClick={() => setCount((count) => count + 1)}>
+        Aumentar a contagem
+      </Button>
+    </StyledDiv>
   );
 }
+
+interface PStyledLink {
+  isBlue?: boolean;
+}
+
+const StyledDiv = styled.div<PStyledLink>`
+  color: ${(props) => (props.isBlue ? 'blue' : 'pink')};
+  font-weight: bold;
+`;
 
 export default App;
